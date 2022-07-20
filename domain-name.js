b695dd9028696031ad1b41d7ac10cca91cbd3946
Ara Kanic <ji.arakani@gmail.com>
2022-07-19 23:58:00 -0400

    Create domain-name.js

---
+++ domain-name.js
@@ -0,0 +1,14 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+function domainName(url){
+  //your code here
+}
+
+console.log(domainName("http://google.com")) // "google"
+console.log(domainName("http://google.co.jp")) // "google"
+console.log(domainName("www.xakep.ru")) // "xakep"
+console.log((domainName("https://youtube.com")) // "youtube"
