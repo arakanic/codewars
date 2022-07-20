2dadb1007fc9ee8c7c4c4e10d12af5cf63de97d7
Ara Kanic <ji.arakani@gmail.com>
2022-07-20 12:17:28 -0400

    Update domain-name.js

--- domain-name.js
+++ domain-name.js
@@ -1,4 +1,4 @@
-// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+// This function parses a URL as a string for its domain and returns it as a string.
 
 // * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
 // * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
